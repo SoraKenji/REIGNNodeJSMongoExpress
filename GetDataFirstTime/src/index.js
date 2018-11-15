678d7b894cf9a1ba03ce2e8c51c080d6ac7aa388
SoraKenji <claudio.torresaviles@gmail.com>
2018-11-15 18:43:06 -0300

    updateCL

--- GetDataFirstTime/src/index.js
+++ GetDataFirstTime/src/index.js
@@ -6,30 +6,36 @@ const request = require('request');
 const urlBase = 'https://hn.algolia.com/api/v1/search_by_date?query=nodejs';
 
 const makeRequest = async () => {
-    await MongoClient.connect(mongourl, async (err, db) => {
-        if (err) throw err;
-        var dbo = db.db("test");
-        await dbo.dropCollection("newsFromPage", function (err, delOK) {
-            if (err) {
-                return;
-            }
-            if (delOK) console.log("Collection deleted");
-        });
-        await dbo.createCollection("newsFromPage", (err, res) => {
+    try {
+        await MongoClient.connect(mongourl, { useNewUrlParser: true }, async (err, db) => {
             if (err) throw err;
-            console.log("Collection created!");
-            return;
-        });
-        let response = await fnRequest();
-        const jsonRes = JSON.parse(response);
-        jsonRes.hits.forEach(element => {
-            dbo.collection("newsFromPage").insertOne(element, (err, res) => {
-                if (err) throw err;
-                console.log("1 document inserted");
+            var dbo = db.db("test");
+            await dbo.dropCollection("newsFromPage", function (err, delOK) {
+                if (err) {
+                    return;
+                }
+                if (delOK) console.log("Collection deleted");
             });
+            await dbo.createCollection("newsFromPage", (err, res) => {
+                if (err) throw err;
+                console.log("Collection created!");
+                return;
+            });
+
+            let response = await fnRequest();
+            const jsonRes = JSON.parse(response);
+            jsonRes.hits.forEach(element => {
+                dbo.collection("newsFromPage").insertOne(element, (err, res) => {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                });
+            });
+            db.close();
         });
-        dbo.close();
-    });
+
+    } catch (err) {
+
+    }
 }
 
 const fnRequest = () => rp(`${urlBase}`)
